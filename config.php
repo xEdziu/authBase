b0731b3982aad3e436c71b8fadccb3d5fd012f54
Adrian Goral <50357817+xEdziu@users.noreply.github.com>
2020-05-11 11:51:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
-  $dbserver = "localhost";
-  $dbuser ="id13505669_zaprogramowani_user";
-  $dbpass ="U(6k<)d*SR}^5Ke-";
-  $dbname ="id13505669_zaprogramowani";
+  $dbserver = "";
+  $dbuser ="";
+  $dbpass ="";
+  $dbname ="";
 
   $link = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
   if ($link->connect_error) {
     die("Connection failed: " . $link->connect_error);
 }
 
-$serwer_smtp = "smtp.wp.pl";
+$serwer_smtp = "";
 $port_smtp = 465;
 
-$smtplog = 'zaprogramowani.g4a@wp.pl';
-$smtppass = 'zaprogramowani123';
+$smtplog = '';
+$smtppass = '';
 
 ?>
