c2e0e84bbb82813b7839e8675f07d1377a061359
Adrian Goral <adrian.goral@gmail.com>
2020-05-12 20:34:01 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html lang="pl">
     <head>
-	    <title>Weryfikacja</title>
+	    <title>Verification</title>
 	    <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
