343dc25990ee049e0eb89dd9508b891a27d081c2
Adrian Goral <adrian.goral@gmail.com>
2020-05-12 20:59:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -17,18 +17,18 @@ $smtppass = '';
 
 function genUUID() {
     return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-        rand(0, 0xffff),
-        rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
 
-        rand(0, 0xffff),
+        mt_rand(0, 0xffff),
 
-        rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x0fff) | 0x4000,
 
-        rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0x3fff) | 0x8000,
 
-        rand(0, 0xffff),
-        rand(0, 0xffff),
-        rand(0, 0xffff)
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff)
     );
 }
 
