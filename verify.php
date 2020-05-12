7b7820868d1de3b6f1e917eb1cfd979c3fd7592f
Adrian Goral <adrian.goral@gmail.com>
2020-05-12 20:30:21 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -16,12 +16,13 @@
  
                     $hash = $link->real_escape_string($_GET['hash']); 
 
-                    $search = $link->query("SELECT hash, active FROM users WHERE hash='$hash' AND active='0'");
+                    //$search = $link->query("SELECT hash, active FROM users WHERE hash='$hash' AND active='0'");
                     
-                    if($search->num_rows > 0) {
+                    //if($search->num_rows > 0) {
+			if ($link->query("UPDATE users SET active='1' WHERE hash='$hash' AND active='0'"));
 
-                        $link->query("UPDATE users SET active='1' WHERE hash='$hash' AND active='0'");
-                        echo '<div>Your account has been verified.<br>You can close this tab.<br>';
+                        //$link->query("UPDATE users SET active='1' WHERE hash='$hash' AND active='0'");
+                        echo '<div>Your account has been verified.<br>You can close this tab.<br></div>';
                     }
                     else {
                         echo '<div>URL address is incorrect or you have already verified your account.</div>';
