4e65c65cf37550dade186438aabc2aa6aa2f931c
Adrian Goral <adrian.goral@gmail.com>
2020-05-12 20:50:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -15,4 +15,21 @@ $port_smtp = 465;
 $smtplog = '';
 $smtppass = '';
 
+function genUUID() {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        rand(0, 0xffff),
+        rand(0, 0xffff),
+
+        rand(0, 0xffff),
+
+        rand(0, 0x0fff) | 0x4000,
+
+        rand(0, 0x3fff) | 0x8000,
+
+        rand(0, 0xffff),
+        rand(0, 0xffff),
+        rand(0, 0xffff)
+    );
+}
+
 ?>
